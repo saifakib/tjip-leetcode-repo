b7c1de72deb2a4d41df09d695dccd22100b335b8
saifakib <mdsaifakib@gmail.com>
2023-12-01 12:26:55 +0600

    first bad version one dimentional approach

--- week05/search_a_2d_matrix.cpp
+++ week05/search_a_2d_matrix.cpp
@@ -3,34 +3,26 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int L =  0;
-        int R = matrix.size() - 1;
+        int n = matrix[0].size();
+        int m = matrix.size();
+
+        int L = 0;
+        int R = n*m - 1;
 
         while(L <= R) {
             int M = L + (R - L) / 2;
-            if(matrix[M].front() <= target && matrix[M].back() >= target) {
-                int LI = 0;
-                int RI = matrix[M].size();
-                while(LI <= RI) {
-                    int MI = LI + (RI - LI) / 2;
-                    if(matrix[M][MI] == target) return true;
-                    if(matrix[M][MI] > target) {
-                        RI = MI - 1;
-                    } 
-                    else {
-                        LI = MI + 1;
-                    }
-                }
-                return false;
-            } 
-            if(matrix[M].front() > target) {
-                R = M - 1;
-            } 
-            else {
+            int row = M / n;
+            int col = M % n;
+            if(matrix[row][col] < target) {
                 L = M + 1;
             }
-        };
-
+            else if(matrix[row][col] > target) {
+                R = M - 1;
+            }
+            else {
+                return true;
+            }
+        }
         return false;
     }
 };
