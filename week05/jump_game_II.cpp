c2687d2a40f428b6d677dca94a009c03d2aa054e
saifakib <mdsaifakib@gmail.com>
2023-12-06 00:24:59 +0600

    Jump Game II

---
+++ week05/jump_game_II.cpp
@@ -0,0 +1,20 @@
+// TC: 0(n)  where n is the size of nums
+// MC: 0(1)
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int jump = 0;
+        int currE = 0;
+        int currF = 0;
+        
+        for(int i = 0; i < nums.size() - 1; i++) {
+            currF = max(currF, nums[i] + i);
+            
+            if(i == currE) {
+                jump++;
+                currE = currF;
+            }
+        }
+        return jump;
+    }
+};
