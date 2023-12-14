e8efb48e204ad179f2c1b40fa44925ab15ec7cea
saifakib <mdsaifakib@gmail.com>
2023-12-14 19:34:22 +0600

    Binary Tree Vertical Order Traversal

---
+++ week06/binary_tree_vertical_order_traversal.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> verticalOrder(TreeNode* root) {
+        map<int, vector<int>> store;
+        queue<pair<TreeNode*, int>> Q;
+        if(root != nullptr) Q.push({root, 0});
+
+        while(!Q.empty()) {
+            int levelSize = Q.size();
+            while(levelSize--) {
+                auto [node, val] = Q.front(); Q.pop();
+                store[val].push_back(node -> val);
+                if(node -> left) Q.push({node -> left, val - 1});
+                if(node -> right) Q.push({node -> right, val + 1});
+            }
+        }
+        vector<vector<int>>vOrder;
+        for(auto i:store) {
+            vOrder.push_back(i.second);
+        }
+        return vOrder;
+    }
+};
